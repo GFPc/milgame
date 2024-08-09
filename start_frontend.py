cd397c5ed00db43c654469948adc4ed41e340d25
GFP <69209530+GFPc@users.noreply.github.com>
2024-08-09 23:20:38 +0700

    Update start_frontend.py

--- start_frontend.py
+++ start_frontend.py
@@ -2,7 +2,10 @@ from threading import Thread
 
 def start_frontend():
     print("gf-point: starting front")
-    os.system("cd frontend ; yarn run start")
-    
+    # Run a command using os.system
+    command = "cd frontend ; yarn run start"
+    os.system(command)
+    result = os.popen(command).read()
+    print("Output from command:", result)
 
 Thread(target=start_frontend, daemon=True).start()
