8272040bd47c09fee6a13af9d21d1e78cc5e6d54
GFP <69209530+GFPc@users.noreply.github.com>
2024-08-27 13:40:37 +0700

    Update start_frontend.py

--- start_frontend.py
+++ start_frontend.py
@@ -3,6 +3,8 @@ import os
 def start_frontend():
     
     print("gf-point: starting front")
+    os.system("python3 -m pip freeze")
+    print("PIP FREEZE RESULT:",os.popen('python3 -m pip freeze').read())
     # Run a command using os.system
     command = "cd frontend ; yarn run start"
     os.system(command)
